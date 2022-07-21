df9f0131603c9f5a1b27d5244ee68443e0240bc3
Nikola Kušen <nikola.kusen@gmail.com>
2022-07-21 14:55:52 +0200

    Add Thymeleaf template for Book list

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,6 +1,6 @@
 package guru.springframework.spring5webapp.controllers;
 
-import guru.springframework.spring5webapp.repositories.BookRepositoy;
+import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,17 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BookController {
 
-    private final BookRepositoy bookRepositoy;
+    private final BookRepository bookRepository;
 
-    public BookController(BookRepositoy bookRepositoy) {
-        this.bookRepositoy = bookRepositoy;
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
     @RequestMapping("/books")
     public String getBooks(Model model) {
 
-        model.addAttribute("books", bookRepositoy.findAll());
+        model.addAttribute("books", bookRepository.findAll());
 
-        return "books";
+        return "books/list";
     }
 }
